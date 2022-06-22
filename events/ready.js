77f14b28e928b6b25f8890115cea4effa59732ad
cat <25059103+cat-marin@users.noreply.github.com>
2022-06-22 15:05:14 -0400

    remove reload and consolidate into ready event

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,29 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { bot } = require('../config.json');
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
+	execute(client, commands) {
 		console.log(`Logged in as ${client.user.tag}!`);
+		client.user.setPresence({activities: [{ name: `${bot.activity}` }], status: 'online' });
 		console.log(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`);
+		// command reload start
+		const CLIENT_ID = client.user.id;
+		const rest = new REST({
+			version: "9",
+		}).setToken(bot.token);
+
+		(async () => {
+			try {
+				await rest.put(Routes.applicationCommands(CLIENT_ID), {
+					body: commands,
+				});
+				console.log("Registered commands!");
+			} catch (err) {
+				if (err) console.error(err);
+			}
+		})();
 	},
 };
