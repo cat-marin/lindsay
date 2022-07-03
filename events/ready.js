bab018504e16447b14316403fe2e1f13ce73e05a
cat <25059103+cat-marin@users.noreply.github.com>
2022-07-03 16:56:14 -0400

    add module list on ready event

--- events/ready.js
+++ events/ready.js
@@ -11,17 +11,20 @@ module.exports = {
 		setInterval(activity => {client.user.setPresence({activities: [{ name: `${bot.activity}` }], status: 'online' }) }, 3600000);
 		console.log(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`);
 		// command reload start
+		if(!bot.moduleReload) return console.log("Module Reload Disabled");
 		client.commands.set([]);
 		const CLIENT_ID = client.user.id;
+		const GUILD_ID = bot.guildId;
 		const rest = new REST({
 			version: "9",
 		}).setToken(bot.token);
 
 		(async () => {
 			try {
-				await rest.put(Routes.applicationCommands(CLIENT_ID), {
+				await rest.put(Routes.applicationCommands(CLIENT_ID, GUILD_ID), {
 					body: commands,
 				});
+				console.log(commands);
 				console.log("Reloaded commands!");
 			} catch (err) {
 				if (err) console.error(err);
